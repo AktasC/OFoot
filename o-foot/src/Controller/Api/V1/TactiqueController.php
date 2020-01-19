c5e84c6783517f18c08e042454cad284f7589c81
Berenger-Benoit <berenger.benoit@gmail.com>
2020-01-19 18:24:59 +0100

    Added methods Api tactiqueController

--- o-foot/src/Controller/Api/V1/TactiqueController.php
+++ o-foot/src/Controller/Api/V1/TactiqueController.php
@@ -5,15 +5,50 @@ namespace App\Controller\Api\V1;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+ /**
+ * @Route("/api/v1/tactiques", name="api_v1_tactiques_")
+ */
 class TactiqueController extends AbstractController
 {
-    /**
-     * @Route("/api/v1/tactique", name="api_v1_tactique")
+   
+      /**
+     * @Route("/", name="list", methods={"GET"})
      */
-    public function index()
+    public function list()
     {
-        return $this->render('api/v1/tactique/index.html.twig', [
-            'controller_name' => 'TactiqueController',
-        ]);
+        return;
+    }
+
+    /**
+     * @Route("/{id}", name="show", requirements={"id": "\d+"}, methods={"GET"})
+     */
+    public function show()
+    {
+        return;
+    }
+
+    /**
+     * @Route("/", name="new", methods={"POST"})
+     */
+    public function new()
+    {
+        return;
+    }
+
+    /**
+     * @Route("/{id}", name="edit", requirements={"id": "\d+"}, methods={"PUT"})
+     */
+    public function edit()
+    {
+        return;
+    }
+
+    /**
+     * @Route("/{id}", name="list", requirements={"id": "\d+"}, methods={"DELETE"})
+     */
+    public function delete()
+    {
+        return;
     }
 }
