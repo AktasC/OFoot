f1f1db866a21b5d52a2650f09729fbcb878e33f3
Berenger-Benoit <berenger.benoit@gmail.com>
2020-01-20 10:21:10 +0100

    edit name method delete

--- o-foot/src/Controller/Api/V1/UserController.php
+++ o-foot/src/Controller/Api/V1/UserController.php
@@ -45,7 +45,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="list", requirements={"id": "\d+"}, methods={"DELETE"})
+     * @Route("/{id}", name="delete", requirements={"id": "\d+"}, methods={"DELETE"})
      */
     public function delete()
     {
