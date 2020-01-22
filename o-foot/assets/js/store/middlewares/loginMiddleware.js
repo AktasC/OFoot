2345ef526ce3b7589034735cd0608bbb076de09a
Marine Berthier <marine154@hotmail.fr>
2020-01-22 13:42:13 +0100

    frontend:login_page_checking: axios ready to be used

--- o-foot/assets/js/store/middlewares/loginMiddleware.js
+++ o-foot/assets/js/store/middlewares/loginMiddleware.js
@@ -1,12 +1,37 @@
 import axios from 'axios';
+import qs from 'qs';
 import { CONNECT_USER } from '../reducer/loginForm';
 import { logUser } from '../reducer/user';
 
 const loginMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case CONNECT_USER:
-    const actionLogUser = logUser();
-    store.dispatch(actionLogUser);
+       /* const {
+            EmailValue,
+            PasswordValue,
+            }=store.getState().loginForm;
+
+            const requestBody = {
+                'login_form[email]': EmailValue,
+                'login_form[password]': PasswordValue,
+              }
+              const config = {
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                }
+              }
+            
+              axios.post('http://localhost:8001/api/v1/login', qs.stringify(requestBody), config)
+
+              .then(function (response) {*/
+                console.log('soumission');        
+                const actionLogUser = logUser();
+                store.dispatch(actionLogUser);
+            /*  })
+              .catch(function (error) {
+                console.log(error);
+              }); */ 
+
     
       break;
     default:
