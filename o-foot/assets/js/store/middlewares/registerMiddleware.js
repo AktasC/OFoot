654386529334f0d60555e684ec9a51c1820d8d5d
Cebrail Aktas <ceb.akt@gmail.com>
2020-01-23 17:45:51 +0100

    Fixed CORS

--- o-foot/assets/js/store/middlewares/registerMiddleware.js
+++ o-foot/assets/js/store/middlewares/registerMiddleware.js
@@ -45,8 +45,7 @@ const registerMiddleware = (store) => (next) => (action) => {
       // .then(function (response) {
 
       // })
-      axios.post('http://localhost:8001/api/register', qs.stringify(requestBody), config)
-      
+      axios.post('/api/register', qs.stringify(requestBody), config)
       .then(function (response) {
         console.log('from then');        
         store.dispatch(signupDone());
