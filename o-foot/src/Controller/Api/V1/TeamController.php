8517daad867205becd861a30788dc5727f7edd47
Live session user <mint@etudiant.oclock.io>
2020-01-27 23:41:37 +0100

    Game edit score : done + test php unit : ok

--- o-foot/src/Controller/Api/V1/TeamController.php
+++ o-foot/src/Controller/Api/V1/TeamController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/api/v1/teams", name="api_v1_teams_")
@@ -196,4 +197,41 @@ class TeamController extends AbstractController
         // On retourne $game au format JSON
         return $this->json('Match bien créé');
     }
+
+
+    /**
+     * @Route("/{team_id}/game/{game_id}/edit-score", name="edit_score_game", methods={"POST"}), requirements={"id": "\d+"}, methods{"POST"})
+     * @ParamConverter("team", options={"mapping": {"team_id": "id"}})
+     * @ParamConverter("game", options={"mapping": {"game_id": "id"}})
+     */
+
+     public function editScoreGame(Game $game, Request $request, SerializerInterface $serializer, Team $team)
+     {
+        // On crée une nouvelle variable $data, qui stocke la sérialisation de l'entité Game en Json
+        $data = $serializer->deserialize($request->getContent(), 'App\Entity\Game', 'json');
+
+        // On indique à $game quels champs nous aimerions créer grâce aux méthodes ->Set récupéré dans l'entité $game
+        // On associe les méthodes get de chaque champs afin de récupérer le champs à créer
+        // On récupére team_id en ajoutant en paramêtre de la fonction l'entité Team
+        $game
+            ->setTeam($team)
+            ->setAssistGame($data->getAssistGame())
+            ->setGoalConcededGame($data->getGoalConcededGame())
+            ->setGoalScoredGame($data->getGoalScoredGame())
+            ->setRedCardGame($data->getRedCardGame())
+            ->setYellowCardGame($data->getYellowCardGame())
+            ->setUpdatedAt(new \DateTime);
+
+        // On récupére l'EntityManager
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // On persiste l'entité $game
+        $entityManager->persist($game);
+
+        // On flushe tout ce qui a été persisté avant pour être sûr que cela soit enregistré en base de données
+        $entityManager->flush();
+
+        // On retourne $game au format JSON
+        return $this->json('Score du match modifié ');
+     }
 }
