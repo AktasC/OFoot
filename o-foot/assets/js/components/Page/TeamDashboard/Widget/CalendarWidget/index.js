b3d10c2e37397fed96cb7bc3068fc052405cf382
MkDs17 <mickael.rassicot@gmail.com>
2020-01-27 14:46:20 +0100

    front/dashboard_team WIP

--- o-foot/assets/js/components/Page/TeamDashboard/Widget/CalendarWidget/index.js
+++ o-foot/assets/js/components/Page/TeamDashboard/Widget/CalendarWidget/index.js
@@ -9,35 +9,46 @@ import calendarData from './calendar.json';
 
 const CalendarWidget = () => {  
 
+  
+  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+
   return (
     
-      <div id="calendarWidget">        
+    <div id="calendarWidget">        
 
-        <Container>
-          {calendarData.event.map((event, i) => (          
-                                   
-            <Row key={event.id} className="event-card">
-              <Col>
-                <div className="date">
-                  {event.date_time_match}
-                </div>
-                <div className="event">
-                  {event.event}
-                </div>
-              </Col> 
-              Je participe à cet évènement :
+      <Container>
+        {calendarData.event.map((event, i) => {
+
+          const event_date = event.date_time_match;
+          const event_date_formatted = new Date(`${event_date}`);
+          console.log(event_date_formatted);
+          console.log(event_date_formatted.toLocaleDateString('fr-FR', options));
+
+          <Row key={event.id} className={`event-card ${event.event.toLowerCase()}`}>
+            <Col>
+              <div className="date">
+                {event.date_time_match.toLocaleDateString('fr-FR', options)}
+              </div>
+              <div className="event">
+                {event.event}
+              </div>
+              <div className="opponent">
+                {event.opponent_team}
+              </div>
+            </Col>            
+            <Col className="participate-col">
+              <Col className="participate-text">Je participe :</Col>
+
               <Col className="buttons-col">                
                 <Button variant="success">Oui</Button>
                 <Button variant="danger">Non</Button>
-              </Col>              
-            </Row> 
-          
-          ))}
-          
-        </Container>
-          
-      </div>    
-    
+              </Col> 
+            </Col>             
+          </Row>           
+        })}          
+      </Container>          
+    </div> 
+
   )
 };
 
