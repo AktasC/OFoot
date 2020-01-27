a296022a98adb7553643dd1d04d721286c5da472
MkDs17 <mickael.rassicot@gmail.com>
2020-01-27 15:29:54 +0100

    front/ dashboard_team WIP

--- o-foot/assets/js/components/Page/TeamDashboard/Widget/CalendarWidget/index.js
+++ o-foot/assets/js/components/Page/TeamDashboard/Widget/CalendarWidget/index.js
@@ -10,7 +10,7 @@ import calendarData from './calendar.json';
 const CalendarWidget = () => {  
 
   
-  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minutes: '2-digit' };
 
   return (
     
@@ -19,32 +19,32 @@ const CalendarWidget = () => {
       <Container>
         {calendarData.event.map((event, i) => {
 
-          const event_date = event.date_time_match;
-          const event_date_formatted = new Date(`${event_date}`);
-          console.log(event_date_formatted);
-          console.log(event_date_formatted.toLocaleDateString('fr-FR', options));
+          let event_date = event.date_time_match;        
+          let event_date_formatted = new Date(`${event_date}`);  
 
-          <Row key={event.id} className={`event-card ${event.event.toLowerCase()}`}>
-            <Col>
-              <div className="date">
-                {event.date_time_match.toLocaleDateString('fr-FR', options)}
-              </div>
-              <div className="event">
-                {event.event}
-              </div>
-              <div className="opponent">
-                {event.opponent_team}
-              </div>
-            </Col>            
-            <Col className="participate-col">
-              <Col className="participate-text">Je participe :</Col>
+          return (
+            <Row key={event.id} className={`event-card ${event.event.toLowerCase()}`}>
+              <Col>
+                <div className="date">
+                  {event_date_formatted.toLocaleDateString('fr-FR', options)}
+                </div>
+                <div className="event">
+                  {event.event}
+                </div>
+                <div className="opponent">
+                  {event.opponent_team}
+                </div>
+              </Col>            
+              <Col className="participate-col">
+                <Col className="participate-text">Je participe :</Col>
 
-              <Col className="buttons-col">                
-                <Button variant="success">Oui</Button>
-                <Button variant="danger">Non</Button>
-              </Col> 
-            </Col>             
-          </Row>           
+                <Col className="buttons-col">                
+                  <Button variant="success">Oui</Button>
+                  <Button variant="danger">Non</Button>
+                </Col> 
+              </Col>             
+            </Row> 
+          )         
         })}          
       </Container>          
     </div> 
