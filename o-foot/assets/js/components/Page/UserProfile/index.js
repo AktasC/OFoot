3f12a0dc90c541163df59abbc1ca8824f26e5769
Marine Berthier <marine154@hotmail.fr>
2020-01-30 14:47:58 +0100

    frontend: bug infosObj fixed

--- o-foot/assets/js/components/Page/UserProfile/index.js
+++ o-foot/assets/js/components/Page/UserProfile/index.js
@@ -23,56 +23,56 @@ class UserProfile extends React.Component {
     const { userInformations } = this.props
     // Récupère les informations sur le joueurs depuis le state userProfil sous forme d'Objet
     // Transforme l'objet récupéré en array
-    var infosArray = Object.values(userInformations);
+    /* var infosArray = Object.values(userInformations); */
     console.log(userInformations);
-    console.log(infosArray );
+    /* console.log(infosArray ); */
     return (
       <div id="userProfile">    
 
         <Container key="1">      
-          {infosArray.map((user, i) => (          
-            <div key={i}>
+               
+            <div>
               <div className="sweatband">
-                <h2>Welcome {user.first_name} !</h2>
+                <h2>Welcome {userInformations.first_name} !</h2>
               </div>          
               <Row className="">
                 <Col lg={12} md={12} sm={12} xs={12}> 
                   <Card>
                     <Card.Header >MES INFORMATIONS</Card.Header>
                     <Card.Body>
-                      <Avatar className="avatar-custom" src={user.picture_user} size="100" round={true} />
-                      <Card.Title>{user.last_name} {user.first_name} </Card.Title>
+                      <Avatar className="avatar-custom" src={userInformations.picture_user} size="100" round={true} />
+                      <Card.Title>{/* {userInform.last_name} {user.first_name} */} </Card.Title>
                       <Card.Text>
-                      Mail : {user.email} 
+                      Mail : {userInformations.email} 
                       </Card.Text>                
                     </Card.Body>
                   </Card>
                 </Col>            
               </Row>
             </div>
-          ))} 
+          
 
         </Container>
 
         <Container>
-      <Tabs defaultActiveKey="teams" id="uncontrolled-tab-example">
-        <Tab eventKey="teams" title="Mes équipes">
-          {/* On mettra ici notre composant ShowMyTeams */}
-          {infosArray.map((user, i) => (  
-            <ShowMyTeams {...user.team}/>
-          ))}
-        </Tab>
-        <Tab eventKey="edit-profil" title="Éditer mon profil">
-          {/* On mettra ici notre composant EditProfil */}
-          
-          <UserEdit />
-        
-        </Tab>
-        <Tab eventKey="edit-password" title="Éditer mon mot de passe">
-          {/* On mettra ici notre composant EditPassword */}
-          <PasswordEdit />
-        </Tab>
-      </Tabs> 
+          <Tabs defaultActiveKey="teams" id="uncontrolled-tab-example">
+            <Tab eventKey="teams" title="Mes équipes">
+              {/* On mettra ici notre composant ShowMyTeams */}
+              {userInformations.teams != undefined && userInformations.teams.map((user, i) => (  
+                <ShowMyTeams {...user.team}/>
+              ))}
+            </Tab>
+            <Tab eventKey="edit-profil" title="Éditer mon profil">
+              {/* On mettra ici notre composant EditProfil */}
+              
+              <UserEdit />
+            
+            </Tab>
+            <Tab eventKey="edit-password" title="Éditer mon mot de passe">
+              {/* On mettra ici notre composant EditPassword */}
+              <PasswordEdit />
+            </Tab>
+          </Tabs> 
         </Container>
 
       </div>  
