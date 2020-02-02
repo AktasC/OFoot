c80125a5d4607a68200ebe942fc5f7826c635a0f
Live session user <mint@etudiant.oclock.io>
2020-02-03 00:24:21 +0100

    Structure page

--- o-foot/assets/js/components/Page/WhoAreWe/index.js
+++ o-foot/assets/js/components/Page/WhoAreWe/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Import scss
 import './who-are-we.scss';
@@ -7,8 +8,84 @@ import './who-are-we.scss';
 
 const WhoAreWe = () => (
     
-    <h2 className="title">WhoAreWe</h2>
-     
+    <div id="qui-sommes-nous">
+
+        <header className="header">
+            <h1 className="header-h1">
+                Mas que un club 
+            </h1>
+            <div className="">
+                <div className="header-title">Créateurs O'Foot</div>
+            </div>
+        </header>
+
+        <Container>
+            <Row className="bloc-1">
+                <Col className ="bloc-1-left">
+                    <div className ="">PC</div>
+                    <h2 className ="">Product Owner/Symfony</h2>
+                </Col>
+                <Col className ="bloc-1-center">
+                    <Avatar className="Avatar-member"></Avatar>
+                </Col>
+                <Col className ="bloc-1-right">
+                    <p className =""><strong>Test</strong>Lorem ipsum</p>
+                </Col>
+            </Row>
+
+            <Row className="bloc-2">
+                <Col className ="bloc-2-left">
+                    <div className ="">PC</div>
+                    <h2 className ="">Product Owner/Symfony</h2>
+                </Col>
+                <Col className ="bloc-2-center">
+                    <Avatar className="Avatar-member"></Avatar>
+                </Col>
+                <Col className ="bloc-2-right">
+                    <p className =""><strong>Test</strong>Lorem ipsum</p>
+                </Col>
+            </Row>
+
+            <Row className="bloc-3">
+                <Col className ="bloc-3-left">
+                    <div className ="">PC</div>
+                    <h2 className ="">Product Owner/Symfony</h2>
+                </Col>
+                <Col className ="bloc-3-center">
+                    <Avatar className="Avatar-member"></Avatar>
+                </Col>
+                <Col className ="bloc-3-right">
+                    <p className =""><strong>Test</strong>Lorem ipsum</p>
+                </Col>
+            </Row>
+
+            <Row className="bloc-4">
+                <Col className ="bloc-4-left">
+                    <div className ="">PC</div>
+                    <h2 className ="">Product Owner/Symfony</h2>
+                </Col>
+                <Col className ="bloc-4-center">
+                    <Avatar className="Avatar-member"></Avatar>
+                </Col>
+                <Col className ="bloc-4-right">
+                    <p className =""><strong>Test</strong>Lorem ipsum</p>
+                </Col>
+            </Row>
+
+            <Row className="bloc-5">
+                <Col className ="bloc-5-left">
+                    <div className ="">PC</div>
+                    <h2 className ="">Product Owner/Symfony</h2>
+                </Col>
+                <Col className ="bloc-5-center">
+                    <Avatar className="Avatar-member"></Avatar>
+                </Col>
+                <Col className ="bloc-5-right">
+                    <p className =""><strong>Test</strong>Lorem ipsum</p>
+                </Col>
+            </Row>
+        </Container>
+    </div>
 );
 
 export default WhoAreWe;
