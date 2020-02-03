3b5fb8d681a04bdbcc98f5877834bbabeb6588ab
Live session user <mint@etudiant.oclock.io>
2020-02-03 23:23:59 +0100

    Data présentation team : json

--- o-foot/assets/js/components/Page/WhoAreWe/index.js
+++ o-foot/assets/js/components/Page/WhoAreWe/index.js
@@ -23,15 +23,15 @@ const WhoAreWe = () => (
             <Row>
                 <Col>
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="img/presentation/Marine.jpg" />
+                        <Card.Img className ="img" variant="top" src="img/presentation/Marine.jpg" />
                         <Card.Body>
                             <Card.Title className="title">Marine Berthier</Card.Title>
                             <Card.Text className="text">
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                             </Card.Text>
-                            <Card.Link href="#">Scrum Master</Card.Link>
-                            <Card.Link href="#">React</Card.Link>
+                            <Card.Link className="role" href="#">Scrum Master</Card.Link>
+                            <Card.Link className="langage" href="#">React</Card.Link>
                         </Card.Body>
                     </Card>
                 </Col>
