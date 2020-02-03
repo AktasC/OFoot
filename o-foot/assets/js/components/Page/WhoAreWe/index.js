e1b55fbf98763f70df0d277af4d024a3edfeedda
Live session user <mint@etudiant.oclock.io>
2020-02-03 18:41:05 +0100

    Design page + image : ok

--- o-foot/assets/js/components/Page/WhoAreWe/index.js
+++ o-foot/assets/js/components/Page/WhoAreWe/index.js
@@ -12,7 +12,7 @@ const WhoAreWe = () => (
     <div id="qui-sommes-nous">
         <header className="header">
             <h1 className="header-h1">
-                Mas que un club 
+                Mas que un projecto
             </h1>
             <div className="">
                 <div className="header-title">Créateurs O'Foot</div>
@@ -23,66 +23,66 @@ const WhoAreWe = () => (
             <Row>
                 <Col>
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="img/presentation/Marine.jpg" />
                         <Card.Body>
-                            <Card.Title>Cascao Pedro</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="title">Marine Berthier</Card.Title>
+                            <Card.Text className="text">
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                             </Card.Text>
-                            <Card.Link href="#">Product Owner</Card.Link>
+                            <Card.Link href="#">Scrum Master</Card.Link>
+                            <Card.Link href="#">React</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="img/presentation/Mickael.jpg" />
+                        <Card.Body>
+                            <Card.Title className="title">Rassicot Mickaël</Card.Title>
+                            <Card.Text className="text">
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Lead Front</Card.Link>
+                            <Card.Link href="#">React</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="img/presentation/Cebrail.jpg" />
+                        <Card.Body>
+                            <Card.Title className="title">Aktas Cebrail</Card.Title>
+                            <Card.Text className="text">
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Git Master</Card.Link>
+                            <Card.Link href="#">Symfony / React</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="img/presentation/Berenger.jpg" />
+                        <Card.Body>
+                            <Card.Title className="title">Berenger Benoit</Card.Title>
+                            <Card.Text className="text">
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Lead Back</Card.Link>
                             <Card.Link href="#">Symfony</Card.Link>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="img/presentation/Pedro.jpg" />
                         <Card.Body>
-                            <Card.Title>Cascao Pedro</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Product Owner</Card.Link>
-                            <Card.Link href="#">Symfony</Card.Link>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Cascao Pedro</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Product Owner</Card.Link>
-                            <Card.Link href="#">Symfony</Card.Link>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Cascao Pedro</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Product Owner</Card.Link>
-                            <Card.Link href="#">Symfony</Card.Link>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Cascao Pedro</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="title">Cascao Pedro</Card.Title>
+                            <Card.Text className="text">
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                             </Card.Text>
