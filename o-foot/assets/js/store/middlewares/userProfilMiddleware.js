8c6c607251e60eea0483586e21d9aaac612ed08b
Marine Berthier <marine154@hotmail.fr>
2020-02-03 19:18:20 +0100

    frontend:change branch

--- o-foot/assets/js/store/middlewares/userProfilMiddleware.js
+++ o-foot/assets/js/store/middlewares/userProfilMiddleware.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
 
-<<<<<<< HEAD
 import {
-  USER_PROFIL_INFO, loadInfoFromAxios, MODIFY_INFO, SUBMIT_CHANGE_PASSWORD, emptyInputs, CALENDAR_INFO,
+  USER_PROFIL_INFO, loadInfoFromAxios, MODIFY_INFO, SUBMIT_CHANGE_PASSWORD, emptyInputs,
 } from '../reducer/userProfil';
 import { modifyPassword } from '../reducer/loginForm';
-=======
-import { USER_PROFIL_INFO, loadInfoFromAxios, MODIFY_INFO, SUBMIT_CHANGE_PASSWORD, emptyInputs } from '../reducer/userProfil';
-import { modifyPassword } from '../reducer/loginForm'
->>>>>>> dev
 import { addNotification } from '../addNotification';
-import { loadInfoCalendarFromAxios } from '../reducer/calendar';
 
 const userProfilMiddleWare = (store) => (next) => (action) => {
   const token = localStorage.getItem('token');
@@ -92,56 +86,6 @@ const userProfilMiddleWare = (store) => (next) => (action) => {
         .catch((error) => {
           addNotification('change-not-done');
           store.dispatch(emptyInputs());
-        });
-      break;
-
-    case CALENDAR_INFO:
-      const { currentTeamId } = store.getState().team;
-
-      axios({
-        method: 'get',
-        url: `/api/v1/events/team/35`,
-        headers: { Authorization: `Bearer ${token}` },
-      })
-
-        .then((response) => {
-        // séparation des objets
-          console.log(response);
-          const eventDataGames = response.data[0];
-          const eventDataPractices = response.data[1];
-
-          // modification key date_time_game/practice
-          const objectRenameKeys = require('object-rename-keys');
-
-          const changes = {
-	        date_time_game: 'date_time',
-          };
-
-          const eventDataGamesRename = objectRenameKeys(eventDataGames, changes);
-
-          const changesPractice = {
-        	date_time_practice: 'date_time',
-          };
-
-          const eventDataPracticesRename = objectRenameKeys(eventDataPractices, changesPractice);
-
-          // Fusion des objets
-          const eventDataMix = eventDataGamesRename.concat(eventDataPracticesRename);
-
-          // classement par ordre chronologique
-          const eventDataMixChronological = eventDataMix.sort((a, b) => {
-            if (a.date_time > b.date_time) return 1;
-            if (a.date_time < b.date_time) return -1;
-            return 0;
-          });
-
-          // filtrer par team_id
-          // const event = eventDataMixChronological.filter(x => x.team_id === 15);
-          const actionLoadInfoCalendar = loadInfoCalendarFromAxios(eventDataMixChronological);
-          store.dispatch(actionLoadInfoCalendar);
-        })
-        .catch((error) => {
-          console.log('error from appel appel axios:', error);
         });
       break;
 
