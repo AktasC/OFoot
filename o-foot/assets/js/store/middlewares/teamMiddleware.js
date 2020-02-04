5837565ee3f8c4bee291c9f3e56cda3876386f5f
MkDs17 <mickael.rassicot@gmail.com>
2020-02-04 10:44:55 +0100

    front/invite_player WIP

--- o-foot/assets/js/store/middlewares/teamMiddleware.js
+++ o-foot/assets/js/store/middlewares/teamMiddleware.js
@@ -5,10 +5,11 @@ import { addNotification } from '../addNotification';
 
 const teamMiddleWare = (store) => (next) => (action) => {
 
+  const token = localStorage.getItem('token');
+  const teamId = store.getState().team.currentTeamId;
+
   switch (action.type) {    
-    case PLAYERS_INFOS:
-      const token = localStorage.getItem('token');
-      const teamId = store.getState().team.currentTeamId;
+    case PLAYERS_INFOS:     
       
       axios({
         method: 'get',
@@ -26,27 +27,24 @@ const teamMiddleWare = (store) => (next) => (action) => {
       break;
 
     case INVITE_PLAYER:
-      console.log('coucou depuis middleware:', action.value);
-
-      addNotification('invite-player-success');
-
-      /* const token = localStorage.getItem('token');
-      const teamId = store.getState().team.currentTeamId;
+      console.log('coucou depuis middleware:', action.value);           
       
       axios({
-        method: 'get',
-        url: `/api/v1/teams/${teamId}/players`,
-        headers: { 'Authorization': `Bearer ${token}` }       
+        method: 'post',
+        url: `/api/v1/teams/${teamId}/invite`,
+        headers: { 'Authorization': `Bearer ${token}` },
+        data: {
+          email: action.value,
+        }     
       })
       
       .then(function (response) {        
-        const actionupdatePlayersList = updatePlayersList(response.data);  
-        store.dispatch(actionupdatePlayersList);
+        addNotification('invite-player-success'); 
       })
       .catch(function (error) {
         console.log("error from appel appel axios:", error);
       });         
-      break; */
+      break;
     
     default:
       // par défaut, je laisse passer l'action
